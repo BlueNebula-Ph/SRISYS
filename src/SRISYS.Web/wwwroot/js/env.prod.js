493075e8dd5069391fe281f943645a7d831118e8
VonAerich <von.aerich@gmail.com>
2017-12-25 01:44:33 +0800

    Updated to prod address.

--- src/SRISYS.Web/wwwroot/js/env.prod.js
+++ src/SRISYS.Web/wwwroot/js/env.prod.js
@@ -2,7 +2,7 @@
     window._environment = window.__env || {};
 
     // API url
-    window._environment.baseUrl = 'http://localhost';
+    window._environment.baseUrl = 'http://desktop-ftuldn3:8080';
 
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
