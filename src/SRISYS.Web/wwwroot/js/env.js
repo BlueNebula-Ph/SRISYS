24869f043a46e3f993f6797f644e5221246759d4
von-tan <vont@magenic.com>
2019-05-17 18:01:19 +0800

    Latest updates

--- src/SRISYS.Web/wwwroot/js/env.js
+++ src/SRISYS.Web/wwwroot/js/env.js
@@ -2,7 +2,7 @@
     window._environment = window.__env || {};
 
     // API url
-    window._environment.baseUrl = 'http://localhost:57210';
+    window._environment.baseUrl = 'http://localhost:64644';
 
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
